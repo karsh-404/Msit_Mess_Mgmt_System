45dd97baa62d6e6c7380fe5b7200042b8fbd3960
karsh-404~ <utkarshporwal26@gmail.com~>
2023-01-17 20:09:14 +0530

    linking frontend and backend commit

--- client/src/components/admin/Meal.js
+++ client/src/components/admin/Meal.js
@@ -14,7 +14,7 @@ const Blogs = () => {
   const [blogs, setBlogs] = useState();
   const sendRequest = async () => {
     const res = await axios
-      .get("http://localhost:5000/api/meal")
+      .get("https://msit-backend.onrender.com/api/meal")
       .catch((err) => console.log(err));
     const data = await res.data;
     return data;
